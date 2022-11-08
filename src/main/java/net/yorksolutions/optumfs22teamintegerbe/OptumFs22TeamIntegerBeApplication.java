79c0a7bcbab4379947f33702de998017e0db3d21
Jackson H <jackson.york.solutions@gmail.com>
2022-11-08 11:24:40 -0600

    Init commit backend project

---
+++ src/main/java/net/yorksolutions/optumfs22teamintegerbe/OptumFs22TeamIntegerBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.optumfs22teamintegerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptumFs22TeamIntegerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptumFs22TeamIntegerBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/optumfs22teamintegerbe/OptumFs22TeamIntegerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.optumfs22teamintegerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptumFs22TeamIntegerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
