f792fa9f8ff553c395f2a795fff3c1df96d999c8
Jackson H <jackson.york.solutions@gmail.com>
2022-11-09 10:40:42 -0600

    Updated Categories entity

--- src/main/java/net/yorksolutions/optumfs22teamintegerbe/entity/Categories.java
+++ src/main/java/net/yorksolutions/optumfs22teamintegerbe/entity/Categories.java
@@ -1,4 +1,35 @@
 package net.yorksolutions.optumfs22teamintegerbe.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    public Categories(){}
+
+    public Categories(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
