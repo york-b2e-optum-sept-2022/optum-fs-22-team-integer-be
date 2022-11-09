01dcdc78954297e3222cf63ca8d8106620b9bc9b
Jackson H <jackson.york.solutions@gmail.com>
2022-11-09 16:49:53 -0600

    CouponCode functionality and tests

---
+++ src/main/java/net/yorksolutions/optumfs22teamintegerbe/controller/CouponCodeController.java
@@ -0,0 +1,30 @@
+package net.yorksolutions.optumfs22teamintegerbe.controller;
+
+import net.yorksolutions.optumfs22teamintegerbe.entity.CouponCode;
+import net.yorksolutions.optumfs22teamintegerbe.service.CouponCodeService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/couponcode")
+@CrossOrigin
+public class CouponCodeController {
+
+    CouponCodeService couponCodeService;
+
+    public CouponCodeController(CouponCodeService couponCodeService) {
+        this.couponCodeService = couponCodeService;
+    }
+
+    @PostMapping
+    public CouponCode createCouponCode(@RequestBody CouponCode couponCode) {
+        return this.couponCodeService.create(couponCode);
+    }
+
+    @PutMapping
+    public CouponCode updateCouponCode(@RequestBody CouponCode couponCode) {
+        return this.couponCodeService.update(couponCode);
+    }
+
+    @GetMapping
+    public Iterable<CouponCode> getAllCouponCodes() { return this.couponCodeService.getAllCouponCodes();}
+}
--- src/main/java/net/yorksolutions/optumfs22teamintegerbe/repository/CouponCodeRepository.java
+++ src/main/java/net/yorksolutions/optumfs22teamintegerbe/repository/CouponCodeRepository.java
@@ -1,9 +1,9 @@
 package net.yorksolutions.optumfs22teamintegerbe.repository;
 
-import net.yorksolutions.optumfs22teamintegerbe.entity.Category;
+import net.yorksolutions.optumfs22teamintegerbe.entity.CouponCode;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CouponCodeRepository extends CrudRepository<Category, Long> {
+public interface CouponCodeRepository extends CrudRepository<CouponCode, Long> {
 }
